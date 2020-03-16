7523104bd125ce3881877a2f1a17f6a676372549
Nahuel Mendez <mendez.nahuel@outlook.com>
2020-03-16 13:16:32 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+"use strict";
+
+console.log('Here\'s a hidden message');
+
+let date = new Date();
+let formatDate = date.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
+
